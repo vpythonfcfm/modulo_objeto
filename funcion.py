6607ecc28b0b7d2df592d28ea8eeba05378e9acf
tomasrojasc <tomas.rojas.c@ug.uchile.cl>
2018-06-15 01:14:06 -0300

    commit inicial

--- funcion.py
+++ funcion.py
@@ -41,7 +41,6 @@ def crear_desde_stl(malla):
             triangulo.v1.pos = triangulo.v1.pos.rotate(angle=epsilon)
             triangulo.v2.pos = triangulo.v2.pos.rotate(angle=epsilon)
             triangulo.v0.pos = triangulo.v0.pos.rotate(angle=epsilon)
-        print(tris)
 
 
 
@@ -90,5 +89,36 @@ def rotar_y(vertexDeVpython,theta):
 def rotar_z(vertexDeVpython,theta):
     matrizRot=np.array([[np.cos(theta),-np.sin(theta),0],[np.sin(theta),np.cos(theta),0],[0,0,1]])
     multiplicar_vertex_matriz(vertexDeVpython,matrizRot)
+
+
+
+#lo que viene ahora es para poder alinear los ejes del objeto con los de la
+#escena, la idea es que esto tb esté en el __init__ de la clase
+
+def cuantoRotarCadaEje(variable_stl):
+    '''
+    esta función se encarga de alinear los ejes del modelo importado con los ejes de la escena de vpython
+    la idea es que al momento de generar los vertex se les haga este arreglo para alinearlos y trabajar en
+    un mismo sist de ref.
+
+    :param variable_stl: entra un archivo stl guardado en una variable
+    :return: devuelve un vector con el angulo que hay que girar en cada eje de la figura para alinearla con la escena
+    '''
+    I=variable_stl.get_mass_properties()[2]
+
+    eig0 = np.linalg.eig(I)[1][:, 0]
+    eig1 = np.linalg.eig(I)[1][:, 1]
+    eig2 = np.linalg.eig(I)[1][:, 2]
+
+    X=np.array([1,0,0])
+    Y=np.array([0,1,0])
+    Z=np.array([0,0,1])
+
+    Alpha=np.arccos((X.dot(eig0))/np.linalg.norm(eig0))
+    Beta=np.arccos((Y.dot(eig1))/np.linalg.norm(eig1))
+    Gamma=np.arccos((Z.dot(eig2))/np.linalg.norm(eig2))
+
+    return np.array([-Alpha,-Beta,-Gamma])
+
 
 
